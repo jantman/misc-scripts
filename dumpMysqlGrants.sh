d948a8e812971b81e4dfc5bd0530e81171d9cb95
Jason Antman <jason@jasonantman.com>
2012-05-15 15:33:38 +0000

    adding mysql grants dump script

---
+++ dumpMysqlGrants.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# I got this from Richard Bronosky's (<http://blog.bronosky.com/>)
+# answer <http://serverfault.com/a/13050> to this thread:
+# <http://serverfault.com/questions/8860/how-can-i-export-the-privileges-from-mysql-and-then-import-to-a-new-server>
+# Many thanks to him.
+#
+# The most up-to-date version of this script can be found at:
+# $HeadURL$
+# $LastChangedRevision$
+#
+
+mygrants()
+{
+read -p "PASSWORD: " PASSWD
+mysql -p$PASSWD -B -N $@ -e "SELECT DISTINCT CONCAT(
+'SHOW GRANTS FOR ''', user, '''@''', host, ''';'
+) AS query FROM mysql.user WHERE user NOT IN ('root','phpmyadmin','debian-sys-maint')"  | \
+mysql -p$PASSWD $@ | \
+sed 's/\(GRANT .*\)/\1;/;s/^\(Grants for .*\)/## \1 ##/;/##/{x;p;x;}'
+}
+
+mygrants
