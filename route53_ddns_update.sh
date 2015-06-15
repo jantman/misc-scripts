a5f4f3d7cbb654f7ac4bf3bea5b5d2a81e667ba8
Jason Antman <jason@jasonantman.com>
2015-06-15 07:25:15 -0400

    update route53_ddns_update.sh

--- route53_ddns_update.sh
+++ route53_ddns_update.sh
@@ -5,6 +5,10 @@
 # Assumes you already have cli53 (<https://github.com/barnybug/cli53>)
 # installed and properly configured.
 #
+# Export ROUTE53_ZONE and ROUTE53_RR_NAME environment variables
+# as your route53 Zone Id and Record Set name, respectively.
+#
+#
 #################################################
 # Copyright 2014 Jason Antman <jason@jasonantman.com> <http://www.jasonantman.com>
 # Free for any use provided that patches are submitted back to me.
@@ -13,6 +17,11 @@
 # <https://github.com/jantman/misc-scripts/blob/master/route53_ddns_update.sh>
 #
 # CHANGELOG:
+#
+# * 2015-06-15 Jason Antman <jason@jasonantman.com>
+# - get config from env vars instead of hard-coded
+# - get OLD_WAN_IP from cli53 instead of local cache file
+# - drop TTL to 60s
 #
 # * 2015-05-31 Jason Antman <jason@jasonantman.com>
 # - update for new whatismyip.jasonantman.com
@@ -26,11 +35,6 @@
 #
 #################################################
 
-# CONFIGURATION
-ZONE=<SET THIS TO YOUR ROUTE53 ZONE ID>
-RR=<SET THIS TO YOUR RR NAME>
-# END CONFIGURATION
-
 LOG_TAG=$(basename "$0")
 
 log () {
@@ -41,13 +45,27 @@ log_err () {
     logger -p local7.notice -t $LOG_TAG "$1"
 }
 
-log "Running with ZONE=${ZONE} RR=${RR}"
+if [ -z ${ROUTE53_ZONE+x} ]
+then
+    >&2 echo "${LOG_TAG} - ERROR - ROUTE53_ZONE environment variable not set"
+    log_err "ERROR - ROUTE53_ZONE environment variable not set"
+    exit 1
+fi
+
+if [ -z ${ROUTE53_RR_NAME+x} ]
+then
+    >&2 echo "${LOG_TAG} - ERROR - ROUTE53_RR_NAME environment variable not set"
+    log_err "ERROR - ROUTE53_RR_NAME environment variable not set"
+    exit 1
+fi
+
+log "Running with ZONE=${ROUTE53_ZONE} RR=${ROUTE53_RR_NAME}"
 
 WAN_IP=$(wget -O - -U wget/route53_ddns_update.sh/iponly 'http://whatismyip.jasonantman.com/?format=plain&only=ip')
 log "Found current WAN IP as ${WAN_IP}"
 
 # Get your old WAN IP
-OLD_WAN_IP=$(cat /var/CURRENT_WAN_IP.txt)
+OLD_WAN_IP=$(cli53 rrlist $ROUTE53_ZONE | grep "^${ROUTE53_RR_NAME}[[:space:]]" | awk '{print $5}')
 log "Found old WAN IP as ${OLD_WAN_IP}"
 
 # See if the new IP is the same as the old IP.
@@ -59,6 +77,6 @@ else
     # The IP changed
     log "Deleting current A record"
     set -o pipefail
-    cli53 rrdelete $ZONE $RR A 2>&1 | logger -p local7.info -t "${LOG_TAG}-rrdelete" || logger -p local7.notice -t $LOG_TAG "cli53 rrdelete FAILED."
-    cli53 rrcreate $ZONE $RR A $WAN_IP 2>&1 | logger -p local7.info -t "${LOG_TAG}-rrcreate" && echo $WAN_IP > /var/CURRENT_WAN_IP.txt || logger -p local7.notice -t $LOG_TAG "cli53 rrcreate FAILED."
+    cli53 rrdelete $ROUTE53_ZONE $ROUTE53_RR_NAME A 2>&1 | logger -p local7.info -t "${LOG_TAG}-rrdelete" || logger -p local7.notice -t $LOG_TAG "cli53 rrdelete FAILED."
+    cli53 rrcreate $ROUTE53_ZONE $ROUTE53_RR_NAME A $WAN_IP --ttl 60 2>&1 | logger -p local7.info -t "${LOG_TAG}-rrcreate" && echo $WAN_IP > /var/CURRENT_WAN_IP.txt || logger -p local7.notice -t $LOG_TAG "cli53 rrcreate FAILED."
 fi
