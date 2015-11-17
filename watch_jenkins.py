7149a07733fa7dad8b6395d98fa114702463cae1
Jason Antman <jason@jasonantman.com>
2015-11-17 10:47:48 -0500

    update watch_jenkins.py

--- watch_jenkins.py
+++ watch_jenkins.py
@@ -10,10 +10,6 @@ pip install python-pushover (optional)
 for pushover configuration, see the section on ~/.pushoverrc in the Configuration section:
 http://pythonhosted.org/python-pushover/#configuration
 
-NOTICE: this assumes that you have unauthenticated read access enabled for Jenkins.
-If you need to authenticate to Jekins in order to read job status, see the comment
-in the main() function.
-
 ##################
 
 Copyright 2014 Jason Antman <jason@jasonantman.com> <http://www.jasonantman.com>
@@ -28,22 +24,27 @@ CHANGELOG:
 - initial script
 2014-12-14 jantman:
 - add better links to config docs
+2015-11-15 jantman:
+- switch from optparse to argparse
+- add support for authenticated access
 """
 
 import sys
-import optparse
+import argparse
 import logging
 import re
 import time
 import os
 import datetime
+import getpass
+from io import StringIO
+
+from jenkins import Jenkins, JenkinsException
 
 try:
     from urllib.parser import urlparse
 except ImportError:
     from urlparse import urlparse
-
-from jenkins import Jenkins, JenkinsException
 
 FORMAT = "[%(levelname)s %(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
 logging.basicConfig(level=logging.ERROR, format=FORMAT)
@@ -56,23 +57,27 @@ except ImportError:
     logger.warning("Pushover support disabled; `pip install python-pushover` to enable it")
     have_pushover = False
 
-def main(build_url, sleeptime=15, pushover=False):
+def main(build_url, sleeptime=15, pushover=False, user=None, password=None):
     if pushover and not have_pushover:
         raise SystemExit("ERROR: to use pushover notifications, please `pip install python-pushover` and configure it.")
     job_name, build_no = get_job_name_and_build_number(build_url)
+    logger.info("Job: %s", job_name)
+    logger.info("Build Number: %s", build_no)
     jenkins_url = get_jenkins_base_url(build_url)
     logger.debug("Connecting to Jenkins...")
-    """
-    NOTE: this is using unauthenticated / anonymous access.
-    If that doesn't work for you, change this to something like:
-    j = Jenkins(jenkins_url, 'username', 'password')
-    """
-    j = Jenkins(jenkins_url)
+
+    if user is not None:
+        logger.debug("Connecting to Jenkins as user %s ...", user)
+        j = Jenkins(jenkins_url, user, password)
+    else:
+        logger.debug("Connecting to Jenkins anonymously...")
+        j = Jenkins(jenkins_url)
     logger.debug("Connected.")
+
     if build_no is None:
         jobinfo = j.get_job_info(job_name)
         build_no = jobinfo['nextBuildNumber'] - 1
-        print("Using latest build, #{b}".format(b=build_no))
+        logger.info("Using latest build, #{b}".format(b=build_no))
     build_url = get_formal_build_url(jenkins_url, job_name, build_no)
     print("Watching job {j} #{b} until completion <{u}>...".format(j=job_name, b=build_no, u=build_url))
     while True:
@@ -106,7 +111,7 @@ def notify_pushover(result, job_name, build_no, duration, build_url):
         req = Client().send_message(msg, title=title, priority=0, sound='falling')
     else:
         req = Client().send_message(msg, title=title, priority=0)
-    
+
 def get_job_name_and_build_number(url):
     """
     Shamelessly stolen from twoline-utils by @coddingtonbear
@@ -115,10 +120,15 @@ def get_job_name_and_build_number(url):
 
     with slight modifications for job without build number
     """
+    logger.debug(url)
+    if url.endswith('/console'):
+        url = url[:len(url) - 8]
+        logger.debug("stripped url to: %s", url)
     job_build_matcher = re.compile(
         ".*/job/(?P<job>[^/]+)/((?P<build_number>[^/]+)/.*)?"
     )
-    tmp = job_build_matcher.search(url).groups()
+    tmp = job_build_matcher.search(url)
+    tmp = tmp.groups()
     job = tmp[0]
     if tmp[2] is not None:
         build_no = int(tmp[2])
@@ -150,32 +160,46 @@ def get_jenkins_base_url(url):
 
 def parse_args(argv):
     """ parse arguments/options """
-    p = optparse.OptionParser(usage="usage: %prog [options] build_or_job_url")
+    p = argparse.ArgumentParser()
 
-    p.add_option('-v', '--verbose', dest='verbose', action='store_true', default=False,
-                 help='verbose (debugging) output')
-    p.add_option('-s', '--sleep-time', dest='sleeptime', action='store', type=int, default=15,
-                 help='time in seconds to sleep between status checks; default 15')
+    p.add_argument('-v', '--verbose', dest='verbose', action='store_true',
+                   default=False, help='verbose (debugging) output')
+    p.add_argument('-s', '--sleep-time', dest='sleeptime', action='store',
+                   type=int, default=15, help='time in seconds to sleep '
+                   'between status checks; default 15')
+    p.add_argument('-u', '--user', dest='user', action='store', type=str,
+                   default=None, help='Jenkins username (optional)')
+    p.add_argument('-p', '--password', dest='password', action='store',
+                   type=str,
+                   default=None, help='Jenkins password (optional; if -u/--user'
+                   ' is specified and this is not, you will be interactively '
+                   'prompted')
+
     push_default = False
     if os.path.exists(os.path.expanduser('~/.watch_jenkins_pushover')):
         push_default = True
-    p.add_option('-p', '--pushover', dest='pushover', action='store_true', default=push_default,
-                 help='notify on completion via pushover (default {p}; touch ~/.watch_jenkins_pushover to default to True)'.format(p=push_default))
+    p.add_argument('-P', '--pushover', dest='pushover', action='store_true',
+                   default=push_default, help='notify on completion via '
+                   'pushover (default {p}; touch ~/.watch_jenkins_pushover to '
+                   'default to True)'.format(p=push_default))
 
-    options, args = p.parse_args(argv)
+    p.add_argument('URL', action='store', type=str,
+                   help='Build URL')
 
-    return options, args
+    args = p.parse_args(argv)
+    if args.user is not None and args.password is None:
+        args.password = getpass.getpass("Password for %s Jenkins "
+                                        "user: " % args.user)
+    return args
 
 
 if __name__ == "__main__":
-    opts, args = parse_args(sys.argv[1:])
+    args = parse_args(sys.argv[1:])
 
-    if opts.verbose:
+    if args.verbose:
         logger.setLevel(logging.DEBUG)
     else:
         logger.setLevel(logging.INFO)
 
-    if len(args) < 1:
-        raise SystemExit("ERROR: you must specify a build or job url")
-
-    main(args[0], sleeptime=opts.sleeptime, pushover=opts.pushover)
+    main(args.URL, sleeptime=args.sleeptime, pushover=args.pushover,
+         user=args.user, password=args.password)
