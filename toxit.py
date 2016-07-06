8fb2742aef8b5e88b9bdddfebc18671a8430d05d
Jason Antman <jason@jasonantman.com>
2016-07-05 21:43:01 -0400

    toxit.py - just use tox's parseconfig()

--- toxit.py
+++ toxit.py
@@ -12,6 +12,8 @@ Free for any use provided that patches are submitted back to me.
 
 CHANGELOG:
 2016-07-05 Jason Antman <jason@jasonantman.com>:
+  - gut the whole script and use tox's own parseconfig()
+2016-07-05 Jason Antman <jason@jasonantman.com>:
   - bug fixes
 2016-07-03 Jason Antman <jason@jasonantman.com>:
   - initial version of script
@@ -22,7 +24,11 @@ import os
 import argparse
 import logging
 import subprocess
-from ConfigParser import SafeConfigParser
+try:
+    from tox.config import parseconfig
+except ImportError:
+    sys.stderr.write("ERROR: Could not import tox - is it installed?\n")
+    raise SystemExit(1)
 
 FORMAT = "[%(levelname)s %(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
 logging.basicConfig(level=logging.INFO, format=FORMAT)
@@ -33,100 +39,47 @@ class ToxIt(object):
     """re-run tox commands against an existing environment"""
 
     ignore_commands = [
-        'python --version',
-        'virtualenv --version',
-        'pip --version',
-        'pip freeze'
+        ['python', '--version'],
+        ['virtualenv', '--version'],
+        ['pip', '--version'],
+        ['pip', 'freeze']
     ]
 
     def __init__(self):
-        # in the future we should search for this, or have an arg for it
-        self.toxinidir = os.path.abspath(os.getcwd())
-        logger.debug('toxinidir: %s', self.toxinidir)
-        fpath = os.path.join(self.toxinidir, 'tox.ini')
-        logger.debug('tox path: %s', fpath)
-        if not os.path.exists(fpath):
-            raise Exception("ERROR: %s does not exist" % fpath)
-        logger.debug('Parsing commands per env')
-        self.commands_per_env = self.parse_toxini(fpath)
-        logger.debug('Commands per env: %s', self.commands_per_env)
+        self.commands_per_env = self.parse_toxini()
 
-    def parse_toxini(self, toxini_path):
+    def parse_toxini(self):
         """parse the tox ini, return dict of environments to list of commands"""
-        cp = SafeConfigParser()
-        logger.debug('Reading %s with SafeConfigParser', toxini_path)
-        cp.read(toxini_path)
-        sections = cp.sections()
-        logger.debug('Config sections: %s', sections)
-        if 'tox' not in sections:
-            raise Exception('Error: tox.ini does not have a "tox" section')
-        envs = cp.get('tox', 'envlist')
-        envs = envs.split(',')
-        logger.debug('tox envs: %s', envs)
-        default_cmds = []
+        logger.debug('Calling tox.config.parseconfig()')
+        config = parseconfig(args=[])
+        logger.debug('Config parsed; envlist: %s', config.envlist)
         env_cmds = {}
-        for s in sections:
-            if s == 'tox':
-                continue
-            raw_cmds = cp.get(s, 'commands').split("\n")
-            cmds = []
-            for c in raw_cmds:
-                c = c.strip()
-                if c == '':
+        for envname in config.envlist:
+            bindir = os.path.join(
+                config.envconfigs[envname].envdir.strpath,
+                'bin'
+            )
+            env_cmds[envname] = []
+            for cmd in config.envconfigs[envname].commands:
+                if cmd in self.ignore_commands:
+                    logger.debug('%s - skipping ignored command: %s',
+                                 envname, cmd)
                     continue
-                if c in self.ignore_commands:
-                    logger.debug('%s - skipping ignored command: %s', s, c)
-                    continue
-                if c.startswith('#'):
-                    logger.debug('%s - skipping comment: %s', s, c)
-                    continue
-                cmds.append(c)
-            logger.debug('%s commands: %s', s, cmds)
-            if s == 'testenv':
-                default_cmds = cmds
-                continue
-            if not s.startswith('testenv:'):
-                logger.debug('Ignoring section: %s', s)
-            s = s[8:]
-            env_cmds[s] = cmds
-        for e in envs:
-            if e not in env_cmds:
-                logger.debug('Setting default (testenv) commands for %s', e)
-                env_cmds[e] = default_cmds
+                cmd[0] = os.path.join(bindir, cmd[0])
+                env_cmds[envname].append(cmd)
+            logger.debug('env %s: %s', envname, env_cmds[envname])
         return env_cmds
 
 
     def run_env(self, envname, cmd_list):
         """run a single env; return True (success) or False (failure)"""
-        bindir = os.path.join(self.toxinidir, '.tox', envname, 'bin')
-        if not os.path.exists(os.path.join(bindir, 'python')):
-            logger.debug('No python binary in %s', bindir)
-            logger.critical('Error: %s does not appear to have a '
-                            'working virtualenv', envname)
-            return False
-        for c in cmd_list:
-            cmd = self.fix_command(c, bindir)
+        for cmd in cmd_list:
             logger.info('Running command: %s', cmd)
-            rcode = subprocess.call(cmd, shell=True)
+            rcode = subprocess.call(cmd)
             logger.info('Command exited %s', rcode)
             if rcode != 0:
                 return False
         return True
-
-
-    def fix_command(self, cmd, bindir):
-        """transform a command to run outside tox"""
-        """
-        py.test -rxs -vv --durations=10 -m "acceptance" {posargs} rpymostat
-        rm -Rf {toxinidir}/docs/build/html
-
-        """
-        cmd = cmd.format(
-            posargs='',
-            toxinidir=self.toxinidir
-        )
-        cmd = bindir + '/' + cmd
-        return cmd
 
 
     def run(self, envlist):
