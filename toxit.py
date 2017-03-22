426801dc44e4bdcb4a0515b6579f4883a488aa69
Jason Antman <jason@jasonantman.com>
2017-03-22 06:20:03 -0400

    update toxit to handle passenv and setenv

--- toxit.py
+++ toxit.py
@@ -7,10 +7,13 @@ re-run only the test commands).
 If you have ideas for improvements, or want the latest version, it's at:
 <https://github.com/jantman/misc-scripts/blob/master/toxit.py>
 
-Copyright 2016 Jason Antman <jason@jasonantman.com> <http://www.jasonantman.com>
+Copyright 2016-2016 Jason Antman <jason@jasonantman.com> <http://www.jasonantman.com>
 Free for any use provided that patches are submitted back to me.
 
 CHANGELOG:
+2017-03-22 Jason Antman <jason@jasonantman.com>:
+  - support passenv and setenv
+  - major internal refactor
 2016-10-28 Jason Antman <jason@jasonantman.com>:
   - ignore 'env' in commands
 2016-07-05 Jason Antman <jason@jasonantman.com>:
@@ -49,47 +52,62 @@ class ToxIt(object):
     ]
 
     def __init__(self):
-        self.commands_per_env = self.parse_toxini()
+        self.env_config = self.parse_toxini()
 
     def parse_toxini(self):
         """parse the tox ini, return dict of environments to list of commands"""
         logger.debug('Calling tox.config.parseconfig()')
         config = parseconfig(args=[])
         logger.debug('Config parsed; envlist: %s', config.envlist)
-        env_cmds = {}
+        env_config = {}
         for envname in config.envlist:
             bindir = os.path.join(
                 config.envconfigs[envname].envdir.strpath,
                 'bin'
             )
-            env_cmds[envname] = []
+            env_config[envname] = {
+                'commands': [],
+                'passenv': [x for x in config.envconfigs[envname].passenv],
+                'setenv': {
+                    a: config.envconfigs[envname].setenv.get(a) for a in
+                    config.envconfigs[envname].setenv.keys()
+                }
+            }
             for cmd in config.envconfigs[envname].commands:
                 if cmd in self.ignore_commands:
                     logger.debug('%s - skipping ignored command: %s',
                                  envname, cmd)
                     continue
                 cmd[0] = os.path.join(bindir, cmd[0])
-                env_cmds[envname].append(cmd)
-            logger.debug('env %s: %s', envname, env_cmds[envname])
-        return env_cmds
+                env_config[envname]['commands'].append(cmd)
+            logger.debug('env %s: %s', envname, env_config[envname])
+        return env_config
 
 
-    def run_env(self, envname, cmd_list):
+    def run_env(self, env_config):
         """run a single env; return True (success) or False (failure)"""
-        for cmd in cmd_list:
+        for cmd in env_config['commands']:
             logger.info('Running command: %s', cmd)
-            rcode = subprocess.call(cmd)
+            rcode = subprocess.call(cmd, env=self._make_env(env_config))
             logger.info('Command exited %s', rcode)
             if rcode != 0:
                 return False
         return True
 
+    def _make_env(self, env_config):
+        e = {}
+        for n in env_config['passenv']:
+            if n in os.environ:
+                e[n] = os.environ[n]
+        for k, v in env_config['setenv'].items():
+            e[k] = v
+        return e
 
     def run(self, envlist):
         """run all selected envs"""
         failed = False
         for e in envlist:
-            res = self.run_env(e, self.commands_per_env[e])
+            res = self.run_env(self.env_config[e])
             if not res:
                 failed = True
         if failed:
