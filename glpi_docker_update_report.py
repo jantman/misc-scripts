8f4b2afd2f97e902c193c9c2a3f8288d4bd50244
Jason Antman <jason@jasonantman.com>
2024-08-04 10:52:36 -0400

    working for HTML report only (no email) and Docker Hub only (not GCR or GHCR)

--- glpi_docker_update_report.py
+++ glpi_docker_update_report.py
@@ -46,6 +46,8 @@ Dependencies
 
 Python 3+
 requests
+python-dateutil
+humanize
 
 License
 -------
@@ -57,10 +59,16 @@ import os
 import sys
 import argparse
 import logging
-from typing import Optional, Dict
-import json
+from typing import Optional, Dict, List
+from time import time, sleep
+from datetime import datetime, timezone
+import re
+from collections import defaultdict
+from base64 import b64encode
 
 import requests
+from dateutil.parser import parse
+from humanize import naturaldelta
 
 logging.basicConfig(
     level=logging.WARNING,
@@ -68,9 +76,242 @@ logging.basicConfig(
 )
 logger: logging.Logger = logging.getLogger()
 
+SEMVER_ANYWHERE_RE: re.Pattern = re.compile(r'^.*\d+\.\d+\.\d+.*$')
+
+UNKNOWN_DATE: datetime = datetime.fromtimestamp(1, tz=timezone.utc)
+
+NOW: datetime = datetime.now(tz=timezone.utc)
+
+
+class Image:
+
+    def __init__(self, name: str, namespace: str, repository: str):
+        self.name: str = name
+        self.namespace: str = namespace
+        self.repository: str = repository
+        self.image_versions: Dict[str, 'ImageVersion'] = {}
+        self.tag_dates: Dict[str, datetime] = {}
+        self.newest_tag: Optional[str] = None
+        self.newest_version_tag: Optional[str] = None
+
+    def version(self, tag: str) -> 'ImageVersion':
+        if tag not in self.image_versions:
+            self.image_versions[tag] = ImageVersion(self, tag)
+        return self.image_versions[tag]
+
+    @property
+    def link(self) -> str:
+        return ''
+
+    def link_for_tag(self, tag: str) -> str:
+        return ''
+
+    def update(self):
+        raise NotImplementedError()
+
+
+class DockerHubImage(Image):
+
+    def _do_get(self, url: str) -> requests.Response:
+        logger.debug('GET %s', url)
+        r = requests.get(url)
+        if r.status_code == 429:
+            logger.debug(
+                'Rate limited (429). Headers: %s', r.headers
+            )
+            retry_after = r.headers['Retry-After']
+            logger.error(
+                'Docker rate limiting; GET returned HTTP 429, '
+                'X-Retry-After %s',
+                retry_after
+            )
+            retry_ts = int(retry_after)
+            now = time()
+            duration = retry_ts - now
+            logger.error(
+                'Rate limiting: sleeping for %s seconds', duration
+            )
+            sleep(duration)
+            return self._do_get(url)
+        r.raise_for_status()
+        return r
+
+    @property
+    def link(self) -> str:
+        return ('https://hub.docker.com/repository/docker'
+                f'/{self.namespace}/{self.repository}/')
+
+    def link_for_tag(self, tag: str) -> str:
+        return (f'https://hub.docker.com/r/{self.namespace}/{self.repository}'
+                f'/tags?name={tag}')
+
+    def update(self):
+        logger.info(
+            'Finding newest tag info for Docker Hub image: %s',
+            self.name
+        )
+        url = (f'https://hub.docker.com/v2/namespaces/{self.namespace}/'
+               f'repositories/{self.repository}/tags?page_size=100')
+        # The Docker API returns tags in order, newest to oldest. Page through
+        # tags 100 at a time until we find the newest tag AND the newest
+        # semver-looking tag, or until we run out...
+        while True:
+            resp = self._do_get(url).json()
+            if not self.newest_tag:
+                self.newest_tag = resp['results'][0]['name']
+                self.tag_dates[resp['results'][0]['name']] = parse(
+                    resp['results'][0]['tag_last_pushed']
+                )
+                logger.info(
+                    'Found newest tag as: %s at %s',
+                    self.newest_tag, self.tag_dates[self.newest_tag]
+                )
+            if not self.newest_version_tag:
+                for tag in resp['results']:
+                    if SEMVER_ANYWHERE_RE.match(tag['name']):
+                        self.newest_version_tag = tag['name']
+                        self.tag_dates[tag['name']] = parse(tag['tag_last_pushed'])
+                        logger.info(
+                            'Found newest semver tag as: %s at %s',
+                            self.newest_version_tag,
+                            self.tag_dates[self.newest_version_tag]
+                        )
+                        break
+            if self.newest_tag and self.newest_version_tag:
+                break
+            if not resp['next']:
+                break
+            url = resp['next']
+        # ok, we should have newest tag and newest version tag if they exist
+        logger.info(
+            'Updating in-use tag info for Docker Hub image: %s',
+            self.name
+        )
+        # now we fill in data for the tags we have in use
+        iver: ImageVersion
+        for iver in self.image_versions.values():
+            url = (
+                f'https://hub.docker.com/v2/namespaces/{self.namespace}/'
+                f'repositories/{self.repository}/tags/{iver.tag}'
+            )
+            resp = self._do_get(url).json()
+            self.tag_dates[iver.tag] = parse(resp['tag_last_pushed'])
+            iver.tag_date = self.tag_dates[iver.tag]
+
+
+class GhcrImage(Image):
+
+    @property
+    def link(self) -> str:
+        return (f'https://github.com/{self.namespace}/{self.repository}/'
+                f'pkgs/container/{self.repository}')
+
+    def link_for_tag(self, tag: str) -> str:
+        return (f'https://github.com/{self.namespace}/{self.repository}/'
+                f'releases/tag/{tag}')
+
+    def update(self):
+        logger.error('ERROR: ghcr.io support not implemented!')
+        return  # @TODO implement me
+        logger.info(
+            'Updating tag info for Docker Hub image: %s', self.name
+        )
+        tokurl = f'https://ghcr.io/token?scope=repository:{self.namespace}/{self.repository}:pull'
+        logger.debug('GET %s', tokurl)
+        tok = requests.get(tokurl)
+        tok.raise_for_status()
+        token = tok.json()['token']
+        url = f'https://ghcr.io/v2/{self.namespace}/{self.repository}/tags/list'
+        r = requests.get(
+            url,
+            headers={'Authorization': f'Bearer {token}'}
+        )
+        j = r.json()
+        logger.debug('Response: %s', j)
+        tags = sorted(j['tags'])
+        print(r.json())
+        raise NotImplementedError()
+        logger.info('Found %d tags', len(result))
+        self.tag_dates = {
+            x['name']: parse(x['tag_last_pushed']) for x in result
+        }
+        # super().update()
+
+
+class GcrImage(Image):
+
+    def update(self):
+        logger.error('ERROR: ghcr.io support not implemented!')
+        pass
+
+
+def get_image(name: str) -> Image:
+    orig_name = name
+    cls = DockerHubImage
+    if name.startswith('ghcr.io/'):
+        cls = GhcrImage
+        name = name[8:]
+    if name.startswith('gcr.io/'):
+        cls = GcrImage
+        name = name[7:]
+    if '/' in name:
+        try:
+            namespace, repository = name.split('/')
+        except ValueError:
+            logger.error('Unable to parse image name: %s', name)
+            raise
+    else:
+        namespace = 'library'
+        repository = name
+    return cls(orig_name, namespace, repository)
+
+
+class ImageVersion:
+
+    def __init__(self, image: Image, tag: str):
+        self.image: Image = image
+        self.tag: str = tag
+        self.tag_date: datetime = UNKNOWN_DATE
+        self.vms: List['VirtualMachine'] = []
+
+    def __repr__(self):
+        return f'<ImageVersion(image="{self.image.name}", tag="{self.tag}">'
+
+
+class Computer:
+
+    def __init__(self, _id: int, name: str):
+        self._id: int = _id
+        self.name: str = name
+        self.vms: Dict[str, 'VirtualMachine'] = {}
+
+    def add_vm(self, name: str, imgver: ImageVersion):
+        self.vms[name] = VirtualMachine(
+            self, name, imgver
+        )
+        imgver.vms.append(self.vms[name])
+
+
+class VirtualMachine:
+
+    def __init__(self, computer: Computer, vm_name: str, imgver: ImageVersion):
+        self.computer: Computer = computer
+        self.name: str = vm_name
+        self.imgver: ImageVersion = imgver
+
+    def __str__(self) -> str:
+        return f'<VirtualMachine(name="{self.name}",ImageVersion={self.imgver}>'
+
+
+def th(s):
+    return '<th style="border: 1px solid black;">%s</th>' % s
+
+
+def td(s):
+    return '<td style="border: 1px solid black; padding: 1em;">%s</td>' % s
+
 
 class GlpiDockerReport:
-
     TOKEN_FILE: str = '.glpi_token.json'
 
     def __init__(self):
@@ -89,6 +330,16 @@ class GlpiDockerReport:
                 'to your GLPI API user token. See the docstring at the top of '
                 'this script for details.'
             )
+        if os.environ.get('DOCKER_HUB_TOKEN') is None:
+            raise RuntimeError(
+                'ERROR: You must set the DOCKER_HUB_TOKEN environment variable '
+                'to your Docker Hub API token.'
+            )
+        if os.environ.get('GITHUB_TOKEN') is None:
+            raise RuntimeError(
+                'ERROR: You must set the GITHUB_TOKEN environment variable '
+                'to your GitHub API token.'
+            )
         self._api_url: str = api_url
         if not self._api_url.endswith('/'):
             self._api_url += '/'
@@ -98,6 +349,13 @@ class GlpiDockerReport:
         self._session_token: str = ''
         self._sess: requests.Session = requests.Session()
         self._login()
+        self.computers: Dict[str, Computer] = {}
+        self.images: Dict[str, Image] = {}
+
+    def _get_image(self, name: str) -> Image:
+        if name not in self.images:
+            self.images[name] = get_image(name)
+        return self.images[name]
 
     def _login(self, once: bool = False):
         sess_token: Optional[str] = self._load_token()
@@ -161,15 +419,119 @@ class GlpiDockerReport:
         r.raise_for_status()
         return r.json()
 
-    def run(self):
+    def run(self, html_file_only: bool = False):
         self._get_glpi_data()
-        print(json.dumps(
-            {
-                self._computer_names[x]: self._containers_by_comp[x]
-                for x in sorted(self._containers_by_comp.keys())
-            },
-            sort_keys=True, indent=4
-        ))
+        name: str
+        comp: Computer
+        for name in sorted(self.computers.keys()):
+            comp = self.computers[name]
+            if not comp.vms:
+                continue
+            print(name)
+            vmname: str
+            imgver: ImageVersion
+            for vmname, imgver in sorted(comp.vms.items()):
+                print(f'\t{vmname}\t{imgver}')
+        img: Image
+        rows: List[Dict] = []
+        for img in sorted(self.images.values(), key=lambda x: x.name):
+            #if img.name == 'ghcr.io/onedr0p/exportarr':  # @TODO REMOVE ME
+            #if img.name == 'ghcr.io/jantman/docker-glpi':  # @TODO REMOVE ME
+            img.update()
+            rows.extend(self._rows_for_image(img))
+        html = self._generate_html(rows)
+        logger.info('Writing report to: glpi_docker_update_report.html')
+        with open('glpi_docker_update_report.html', 'w') as fh:
+            fh.write(html)
+        if html_file_only:
+            return
+        raise NotImplementedError()
+
+    def _rows_for_image(self, img: Image) -> List[Dict]:
+        result = []
+        iver: ImageVersion
+        for iver in sorted(img.image_versions.values(),
+                           key=lambda x: x.tag_date):
+            hosts = defaultdict(list)
+            vm: VirtualMachine
+            for vm in iver.vms:
+                hosts[vm.computer.name].append(vm.name)
+            result.append({
+                'Image': img.name,
+                'ImageLink': img.link,
+                'ImageNewestTag': img.newest_tag,
+                'ImageNewestTagLink': img.link_for_tag(img.newest_tag),
+                'ImageNewestTagDate': img.tag_dates.get(img.newest_tag),
+                'ImageNewestVer': img.newest_version_tag,
+                'ImageNewestVerLink': img.link_for_tag(img.newest_version_tag),
+                'ImageNewestVerDate': img.tag_dates.get(img.newest_version_tag),
+                'Tag': iver.tag,
+                'TagLink': img.link_for_tag(iver.tag),
+                'Date': iver.tag_date,
+                'Hosts': dict(hosts),
+            })
+        return result
+
+    def _generate_html(self, rows: List[Dict]) -> str:
+        html = ('<html><head>'
+                '<title>GLPI Docker Update Report</title>'
+                '</head>\n')
+        html += '<body>\n'
+        html += '<h1>GLPI Docker Update Report</h1>\n'
+        html += '<h2>Generated at '
+        html += datetime.now(timezone.utc).astimezone().strftime('%c %Z')
+        html += '</h2>\n'
+        html += ('<table style="border: 1px solid black; '
+                 'border-collapse: collapse;">\n')
+        html += '<thead><tr>'
+        html += th('Image')
+        html += th('Tag')
+        html += th('Age')
+        html += th('Hosts')
+        html += th('Newest Tag')
+        html += th('Newest SemVer Tag')
+        html += '</tr></thead>\n'
+        html += '<tbody>\n'
+        curr_name = ''
+        for row in rows:
+            html += '<tr>'
+            if curr_name != row['Image']:
+                html += td(f'<a href="{row["ImageLink"]}">{row["Image"]}</a>')
+                curr_name = row['Image']
+            else:
+                html += td('&nbsp;')
+            html += td(f'<a href="{row["TagLink"]}">{row["Tag"]}</a>')
+            html += td(naturaldelta(NOW - row['Date']))
+            html += td(
+                '; '.join([
+                    f'{x} ({", ".join(sorted(row["Hosts"][x]))})'
+                    for x in sorted(row['Hosts'].keys())
+                ])
+            )
+            if row["ImageNewestTagDate"]:
+                html += td(
+                    f'<a href="{row["ImageNewestTagLink"]}">{row["ImageNewestTag"]}</a>'
+                    f' ({naturaldelta(NOW - row["ImageNewestTagDate"])} ago)'
+                )
+            else:
+                html += td(
+                    f'<a href="{row["ImageNewestTagLink"]}">{row["ImageNewestTag"]}</a>'
+                    ' (unknown age)'
+                )
+            if row["ImageNewestVerDate"]:
+                html += td(
+                    f'<a href="{row["ImageNewestVerLink"]}">{row["ImageNewestVer"]}</a>'
+                    f' ({naturaldelta(NOW - row["ImageNewestVerDate"])} ago)'
+                )
+            else:
+                html += td(
+                    f'<a href="{row["ImageNewestVerLink"]}">{row["ImageNewestVer"]}</a>'
+                    ' (unknown age)'
+                )
+            html += '</tr>\n'
+        html += '</tbody>\n'
+        html += '</table></body></html>\n'
+        return html
 
     def _get_glpi_data(self):
         comp: dict
@@ -187,13 +549,14 @@ class GlpiDockerReport:
                 )
                 continue
             logger.info('Computer %d (%s)', comp['id'], comp['name'])
-            self._computer_names[comp['id']] = comp['name']
-            self._containers_by_comp[comp['id']] = {}
-            self._do_computer(comp['id'])
+            self._do_computer(comp['id'], comp['name'])
 
-    def _do_computer(self, comp_id: int):
+    def _do_computer(self, comp_id: int, comp_name: str):
+        comp = Computer(comp_id, comp_name)
+        self.computers[comp_name] = comp
         vm: dict
-        for vm in self._api_get_json(f'Computer/{comp_id}/ComputerVirtualMachine/?expand_dropdowns=true'):
+        for vm in self._api_get_json(
+                f'Computer/{comp_id}/ComputerVirtualMachine/?expand_dropdowns=true'):
             if vm.get('virtualmachinetypes_id') != 'docker':
                 logger.debug(
                     'Skip VM with type %s: %d name=%s (comment %s)',
@@ -214,10 +577,14 @@ class GlpiDockerReport:
                     vm.get('virtualmachinestates_id')
                 )
                 continue
-            self._containers_by_comp[comp_id][vm['name']] = vm['comment']
+            name: str
+            tag: str
+            name, tag = vm['comment'].split(':')
+            img: Image = self._get_image(name)
+            ver: ImageVersion = img.version(tag)
+            comp.add_vm(vm['name'], ver)
         logger.info(
-            'Found %d containers on Computer %s',
-            len(self._containers_by_comp[comp_id]), comp_id
+            'Done with computer %s (%d)', comp_name, comp_id
         )
 
 
@@ -226,6 +593,10 @@ def parse_args(argv):
     p.add_argument(
         '-v', '--verbose', dest='verbose', action='store_true',
         default=False, help='verbose output'
+    )
+    p.add_argument(
+        '-H', '--html', dest='html', action='store_true',
+        default=False, help='Just write local HTML report and exit'
     )
     args = p.parse_args(argv)
     return args
@@ -266,4 +637,4 @@ if __name__ == "__main__":
     else:
         set_log_info(logger)
 
-    GlpiDockerReport().run()
+    GlpiDockerReport().run(html_file_only=args.html)
